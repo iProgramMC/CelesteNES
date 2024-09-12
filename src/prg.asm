e0150dfb4374257630593daf609ab88776f82b6f
iProgramInCpp <iprogramincpp@gmail.com>
2024-09-12 17:31:18 +0300

    * Added logo, a bunch more stuff

--- src/prg.asm
+++ src/prg.asm
@@ -10,25 +10,103 @@ ppu_addr    = $2006
 ppu_data    = $2007
 palette_mem = $3F00
 apu_dmc_cfg = $4010
+apu_oam_dma = $4014
 apu_frctr   = $4017
 
 ; Constants
+blank_tile  = $00
 apu_irq_off = $40
+oam_buf_hi  = $07   ; matches the upper bytes of the address of oam_buf
 lastpage    = $FF00
+ctl_irq_off = %00110000
+ctl_irq_on  = %10110000
+def_ppu_msk = %00011110
+
+; Game Modes
+gm_game     = $00
+gm_title    = $01
+gm_titletra = $02   ; title transition
+
+; Variables (RAM: 0x0000 - 0x0800)
+oam_buf     = $0700 ; OAM buffer, flushed every vblank to PPU OAM
+oam_offset  = $0000
+oam_wrhead  = $0001 ; OAM buffer write head
+wr_str_temp = $0002 ; and $0003
+x_crd_temp  = $0004 ; used by oam_putsprite
+y_crd_temp  = $0005 ; used by oam_putsprite
+rng_state   = $0006
+
+player_x    = $0010
+player_y    = $0011
+player_sp_x = $0012 ; subpixel memory X
+player_sp_y = $0013 ; subpixel memory Y
+camera_x    = $0014
+camera_y    = $0015
+scrincoff   = $0016 ; screen increment offset - copied into ppuctrl
+gamemode    = $0017 ; active game mode
+titlectrl   = $0018 ; title control
+
+; NOTE: these addresses can and should be repurposed for in-game
+tl_snow_y   = $0020 ; Y coordinates of the 16 snow particles
+tl_snow_x   = $0030 ; X coordinates of the 16 snow particles
+tl_timer    = $0040
 
 .org $8000
 
-nmi:
-	rti
-
+; ** SUBROUTINE: vblank_wait
+; arguments: none
+; clobbers: A
 vblank_wait:
+	lda #$00
 	bit ppu_status
 	bpl vblank_wait  ; check bit 7, equal to zero means not in vblank
 	rts
 
+; ** SUBROUTINE: rand
+; arguments: none
+; clobbers:  a, x
+; returns:   a - the pseudorandom number
+; desc:      generates a pseudo random number
+rand:
+	lda rng_state
+	asl
+	bcc no_feedback
+	eor #$21
+no_feedback:
+	sta rng_state
+	lda rng_state
+	rts
+
+; ** SUBROUTINE: oam_putsprite
+; arguments:
+;   a - attributes
+;   y - tile number
+;   [x_crd_temp] - y position of sprite
+;   [y_crd_temp] - y position of sprite
+; clobbers:  a, x
+; desc:      inserts a sprite into OAM memory
+oam_putsprite:
+	ldx oam_wrhead  ; load the write head into X
+	pha             ; push the tile number
+	lda y_crd_temp  ; store the Y coordinate into OAM
+	sta oam_buf, x
+	inx
+	tya
+	sta oam_buf, x  ; store the attributes into OAM
+	inx
+	pla
+	sta oam_buf, x  ; store the tile number into OAM
+	inx
+	lda x_crd_temp
+	sta oam_buf, x  ; store the X coordinate into OAM
+	inx
+	stx oam_wrhead
+	rts
+
+; ** SUBROUTINE: load_palette
 ; arguments:
 ;   y - the offset from the last page where the palette resides
-; clobbers: a, x
+; clobbers: A, X
 ; assumes: PPUCTRL increment bit is zero (+1 instead of +32)
 load_palette:
 	lda #$3F
@@ -37,7 +115,7 @@ load_palette:
 	sta ppu_addr
 	ldx #$00
 load_palette_loop:
-	lda lastpage, x
+	lda lastpage, y
 	sta ppu_data
 	inx
 	iny
@@ -45,6 +123,96 @@ load_palette_loop:
 	bne load_palette_loop
 	rts
 
+; ** SUBROUTINE: ppu_nmi_off
+; arguments: none
+; clobbers: A
+ppu_nmi_off:
+	lda scrincoff
+	ora #ctl_irq_off
+	sta ppu_ctrl
+	rts
+
+; ** SUBROUTINE: ppu_nmi_on
+; arguments: none
+; clobbers: A
+ppu_nmi_on:
+	lda scrincoff
+	ora #ctl_irq_on
+	sta ppu_ctrl
+	rts
+
+; ** SUBROUTINE: ppu_rstaddr
+; arguments: none
+; clobbers:  A
+; desc:      writes $2000 to PPUADDR in vblank or after a render disable section
+ppu_rstaddr:
+	lda #$20
+	sta ppu_addr
+	lda #$00
+	sta ppu_addr
+	rts
+
+; ** SUBROUTINE: ppu_loadaddr
+; arguments:
+;   x - low 8 bits of address
+;   y - high 8 bits of address
+; clobbers: none
+; assumes:  none
+; desc:     loads a 16-bit address into PPUADDR
+ppu_loadaddr:
+	sty ppu_addr
+	stx ppu_addr
+	rts
+
+; ** SUBROUTINE: ppu_wrstring
+; arguments:
+;   x - low 8 bits of address
+;   y - high 8 bits of address
+;   a - length of string
+; assumes:  - PPUADDR was programmed to the PPU dest address
+;             writes can happen (in vblank or rendering disabled)
+;           - that the string does not straddle a page
+;             boundary (256 bytes)
+; desc:     copies a string from memory to the PPU
+; clobbers: PPUADDR, all regs
+ppu_wrstring:
+	stx wr_str_temp       ; store the address into a temporary
+	sty wr_str_temp + 1   ; indirection slot
+	ldy #$00
+	tax                   ; A cannot be incremented with 1 instruction
+ppu_wrsloop:              ; so use X for that purpose
+	lda (wr_str_temp), y  ; use that indirection we setup earlier
+	sta ppu_data
+	iny
+	dex
+	bne ppu_wrsloop       ; if X != 0 print another
+	rts
+	
+	
+
+; ** SUBROUTINE: clear_nt
+; arguments: a - high 8 bits of nametable address (20,24,28,2C)
+; clobbers:  a, x, y
+; assumes:   rendering is disabled (not enough bandwidth to clear the entire nametable during vblank)
+; desc:      clears 1KB of RAM in PPU memory with video output disabled
+clear_nt:
+	sta ppu_addr
+	lda #$00
+	sta ppu_addr
+	lda #blank_tile  ; clear all 1K of vram to 0x20 - the blank tile
+	ldx #$00
+	ldy #$00
+inner_loop:
+	sta ppu_data
+	iny
+	bne inner_loop
+	inx
+	cpx #$04
+	bcc inner_loop   ; jump to the inner loop because y==0 guaranteed
+                     ; we didn't branch because carry was set so y==0
+	rts
+
+; ** ENTRY POINT
 reset:
 	sei              ; disable interrupts
 	cld              ; clear decimal flag - not really required
@@ -62,6 +230,7 @@ reset:
 	ldx #$00
 reset_clrmem:
 	; clears all 2KB of work RAM. that includes the zero page.
+	sta $000, x
 	sta $100, x
 	sta $200, x
 	sta $300, x
@@ -77,8 +246,34 @@ reset_clrmem:
 	ldy init_palette - lastpage
 	jsr load_palette ; move palette to palette RAM
 	
-the:jmp the
+	lda #$20         ; clear the two nametables
+	jsr clear_nt
+	lda #$24
+	jsr clear_nt
+	
+	ldy #def_ppu_msk ; show background & sprites
+	sty ppu_mask     ; set the ppu mask
+	ldy #ctl_irq_on  ; set sprite size (8x16) and NMI enable
+	sty ppu_ctrl
+	
+	ldy #gm_title
+	sty gamemode     ; set title screen mode
+	
+	ldy #$ac
+	sty rng_state    ; initialize rng seed
+	
+	jsr vblank_wait  ; one final vblank wait
+	cli
+	
+; ** MAIN LOOP
+main_loop:
+	jsr ppu_nmi_off
+	jsr game_update
+	jsr ppu_nmi_on
+	jsr vblank_wait
+	jmp main_loop
 
+.include "game.inc"
 
 .res lastpage - *, $FF
 init_palette:
@@ -87,9 +282,17 @@ init_palette:
 	.byte $0f,$20,$21,$11 ; blue tiles
 	.byte $0f,$39,$29,$19 ; green tiles
 	.byte $0f,$15,$37,$21 ; player sprite colors
-	.byte $0f,$00,$00,$00 ; unused sprite 1
-	.byte $0f,$00,$00,$00 ; unused sprite 2
-	.byte $0f,$00,$00,$00 ; unused sprite 3
+	.byte $0f,$15,$25,$36 ; red/strawberry sprite
+	.byte $0f,$30,$29,$09 ; green/refill sprite
+	.byte $0f,$20,$10,$00 ; unused sprite 3
+
+; logo data
+logo_row1: .byte $20,$20,$20,$c0,$20,$20,$20
+logo_row2: .byte $c6,$c5,$c1,$c5,$c3,$c4,$c5
+logo_row3: .byte $c7,$c8,$ca,$c9,$cb,$cc,$c9
+logo_row4: .byte $cd,$20,$20,$20,$20,$ce,$cf
+logo_pal:  .byte $aa,$aa,$aa,$aa
+logo_pressstart: .byte "PRESS START"
 
 .res $FFFA - *, $FF
 	.word nmi
