4b59214a0804e2440ff5df8adc5f6e2aa0ffc241
iProgramInCpp <iprogramincpp@gmail.com>
2024-10-30 22:37:49 +0200

    * Update layout a bit.

--- src/prg_dial.asm
+++ src/prg_dial.asm
@@ -1,4 +1,10 @@
 ; Copyright (C) 2024 iProgramInCpp
 
+.segment "DLGRAM"
+d_old_attrs: .res 16  ; copied from VRAM in an NMI
+
+.segment "DLGTEMP"
+d_old_tiles: .res 512 ; copied from VRAM in an NMI
+
 .segment "PRG_DIAL"
 .include "d_update.asm"
