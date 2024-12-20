e3e3155238d33427e3c0dc5ca8566954b823a02a
iProgramInCpp <iprogramincpp@gmail.com>
2024-12-20 18:28:50 +0200

    * Small commit

--- src/level0/roomlist.asm
+++ src/level0/roomlist.asm
@@ -1,4 +1,3 @@
-
 level0:
 	.word level0_music	; music table
 	.word level0_banks	; list of banks for each room
@@ -8,9 +7,8 @@ level0:
 	.word level0_r2
 	.word level0_r3
 	.word level0_r4
-
 level0_banks:
-	.byte prgb_lvl0a
-	.byte prgb_lvl0a
-	.byte prgb_lvl0a
-	.byte prgb_lvl0a
+	.byte prgb_lvl0a	; level0_r1
+	.byte prgb_lvl0a	; level0_r2
+	.byte prgb_lvl0a	; level0_r3
+	.byte prgb_lvl0a	; level0_r4
