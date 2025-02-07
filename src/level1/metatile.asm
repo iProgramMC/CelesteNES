8a0dd8ac32218b459e53a029cb6386132c55f3f3
iProgramInCpp <iprogramincpp@gmail.com>
2025-02-07 10:26:29 +0200

    * Ch1: Move snow tiles back to their old place.
    This fixes the issue where the Cassette Block room in Ch1 turns all
    the snow into dream blocks.

--- src/level1/metatile.asm
+++ src/level1/metatile.asm
@@ -15,22 +15,22 @@
 ;metatiles:
 .align $100
 	.byte $00 ; Air
-	.byte $80,$84,$90,$94 ; Dirt Corners
-	.byte $81,$82,$83     ; Dirt Tops
-	.byte $91,$92,$93     ; Dirt Bottoms
-	.byte $87,$97,$88,$87 ; Dirt Left
-	.byte $89,$99,$8A,$9A ; Dirt Right
-	.byte $8B,$8C,$8D,$8E,$9B,$9C,$9D,$7A ; Dirt Middles
-	.byte $8F,$98         ; Dirt Left, Right Ends
-	.byte $60,$61,$72,$73 ; Snow Corners
-	.byte $6B,$6C,$6D     ; Snow Tops
-	.byte $74,$75,$76     ; Snow Bottoms
-	.byte $70,$62         ; Snow Left
-	.byte $71,$63         ; Snow Right
-	.byte $64,$65,$66,$7E,$6F,$6E,$7B,$7C,$7D ; Snow Middles
-	.byte $67,$68,$77,$78 ; Snow In-corners
-	.byte $85,$86,$95,$96 ; Dirt In-corners
-	.byte $69,$6A,$79     ; Jump Through
+	.byte $60,$64,$70,$74 ; Dirt Corners
+	.byte $61,$62,$63     ; Dirt Tops
+	.byte $71,$72,$73     ; Dirt Bottoms
+	.byte $67,$77,$68,$67 ; Dirt Left
+	.byte $69,$79,$6A,$7A ; Dirt Right
+	.byte $6B,$6C,$6D,$6E,$7B,$7C,$7D,$9A ; Dirt Middles
+	.byte $6F,$78         ; Dirt Left, Right Ends
+	.byte $80,$81,$92,$93 ; Snow Corners
+	.byte $8B,$8C,$8D     ; Snow Tops
+	.byte $94,$95,$96     ; Snow Bottoms
+	.byte $90,$82         ; Snow Left
+	.byte $91,$83         ; Snow Right
+	.byte $84,$85,$86,$9E,$8F,$8E,$9B,$9C,$9D ; Snow Middles
+	.byte $87,$88,$97,$98 ; Snow In-corners
+	.byte $65,$66,$75,$76 ; Dirt In-corners
+	.byte $89,$8A,$99     ; Jump Through
 	.byte $C8             ; Spikes
 	.byte $C4,$C5,$C6,$C7 ; Grass Decals
 	.byte $A0,$A4,$B0,$B4 ; Cement Corners
@@ -43,7 +43,7 @@
 	.byte $A7,$A8,$B7,$B8 ; Cement Corners
 	.byte $C0,$C1,$C2,$C3 ; Girders
 	.byte $C9,$CA,$CB     ; Spikes
-	.byte $69,$6A,$79     ; Grey Jump Through
+	.byte $89,$8A,$99     ; Grey Jump Through
 	.byte $F4,$F5         ; Memorial
 	.byte $F8,$D9,$F9,$E7 ; Memorial
 	.byte $D0,$D1,$D2,$D3 ; Memorial
@@ -54,7 +54,7 @@
 	.byte $E4,$E5,$E6,$E8,$E9 ; Memorial
 	.byte $DA,$DB,$DC,$DD ; Campfire
 	.byte $C8,$C9,$CA,$CB ; Spikes (Blue)
-	.byte $7F,$CC,$CD,$CE,$CF,$DF ; Snow
+	.byte $9F,$CC,$CD,$CE,$CF,$DF ; Snow
 	; $99
 	.byte $21,$04,$20,$24,$02,$22,$01,$05 ; Cassette Blocks 1 (UL UR DL DR U D L R)
 	.byte $08,$0C,$28,$2C,$0A,$2A,$09,$0D ; Cassette Blocks 2 (UL UR DL DR U D L R)
